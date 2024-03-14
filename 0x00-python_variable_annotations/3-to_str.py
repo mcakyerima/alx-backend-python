92cde0220f252adaa457db7db6f329165b2917cf
mcakyerima <mcakyerima@gmail.com>
2024-03-15 00:29:37 +0100

    ✅ finished task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Returns the string representation of a float number."""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of a float number.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        str: The string representation of the input number.
+    """
+    return str(n)
