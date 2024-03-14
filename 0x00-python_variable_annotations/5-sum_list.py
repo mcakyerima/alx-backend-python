d213bdaf8aa0e9ed713eed521629b5817d65740b
mcakyerima <mcakyerima@gmail.com>
2024-03-15 00:34:10 +0100

    ✅ finished task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Calculates the sum of a list of floats."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Calculate the sum of a list of floats.
+
+    Args:
+        input_list (list[float]): The list of floats to sum.
+
+    Returns:
+        float: The sum of the input list of floats.
+    """
+    return sum(input_list)
