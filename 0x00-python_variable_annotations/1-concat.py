c6ae2d05c32663990e2191a50b0523e3796c4f07
mcakyerima <mcakyerima@gmail.com>
2024-03-15 00:22:31 +0100

    ✅ finished task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" concats and returns 2 strings """
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result.
+
+    Args:
+        str1 (str): The first string to concatenate.
+        str2 (str): The second string to concatenate.
+
+    Returns:
+        str: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
