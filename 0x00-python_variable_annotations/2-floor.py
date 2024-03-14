647dedb2efd9e85db759134fbb57620e92a7fa47
mcakyerima <mcakyerima@gmail.com>
2024-03-15 00:27:47 +0100

    ✅ finished task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Returns the floor of a float number."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float number.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        int: The floor value of the input number.
+    """
+    return math.floor(n)
