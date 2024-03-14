e82ec42680d39e6f2fd37a68ff259d55528bd672
mcakyerima <mcakyerima@gmail.com>
2024-03-15 00:49:28 +0100

    ✅ finished task 6 & 7

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """Calculates the sum of a list of integers and floats."""
+
 from typing import List, Union
 
 
@@ -9,9 +10,10 @@ def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     Calculate the sum of a list of integers and floats.
 
     Args:
-        mxd_lst (list[typing.Union[int, float]]): The list of integers and floats to sum.
+        mxd_lst (List[Union[int, float]]): The list of int and floats to sum.
 
     Returns:
         float: The sum of the input list of integers and floats.
     """
+
     return float(sum(mxd_lst))
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """Converts a string and an int/float to a tuple."""
+
 from typing import Union, Tuple
 
 
@@ -13,6 +14,7 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         v (Union[int, float]): The int or float value.
 
     Returns:
-        Tuple[str, float]: A tuple containing the string k and the square of v as a float.
+        Tuple[str, float]: A tuple containing the string k and the square of v
     """
+
     return (k, float(v**2))
