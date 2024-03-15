4f61c06f9c72e04e2c0c0d7818b867f7022889b4
mcakyerima <mcakyerima@gmail.com>
2024-03-15 01:33:59 +0100

    🔧 fixed bug in task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 """Calculates the sum of a list of floats."""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Calculate the sum of a list of floats.
 
@@ -13,4 +14,4 @@ def sum_list(input_list: list[float]) -> float:
     Returns:
         float: The sum of the input list of floats.
     """
-    return sum(input_list)
+    return float(sum(input_list))
