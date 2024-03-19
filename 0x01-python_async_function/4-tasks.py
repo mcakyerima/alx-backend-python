7136a6de6da21ae5c38b519bc74838df451f4cce
mcakyerima <mcakyerima@gmail.com>
2024-03-19 04:11:52 +0100

    ✅ finshed task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''Task 4 module '''
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Executes multiple coroutines concurrently using asyncio.Tasks.
+
+    Args:
+        n (int): The number of coroutines to execute.
+        max_delay (int): The maximum delay for each coroutine.
+
+    Returns:
+        List[float]: List of delays (float values) in asc order.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return sorted(await asyncio.gather(*tasks))
