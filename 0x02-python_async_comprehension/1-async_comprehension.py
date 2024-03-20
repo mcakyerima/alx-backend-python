3a7e05a1f403d2b98959c23bc3bac9b7f81ce33f
mcakyerima <mcakyerima@gmail.com>
2024-03-20 04:51:59 +0100

    ✅ fixed typo in shebang

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,7 @@ import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """ Collects 10 random numbers using async comprehension over async_generator 
 
